0c261b5d831a5322ffa1d76910c23ce7b7652a66
S664470 <shahialtayeb@gmail.com>
2023-06-08 13:13:21 -0700

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,26 +1,39 @@
 #include <stdlib.h>
 
+/**
+ * File: hash_table_create
+ * Auth: Shahinda Altayeb
+ * Dias: Function to create hash table
+ * @size: Is the size of the array
+ * Return: Pointer to a new hash table or NULL if something wrong
+ */
+
 typedef struct hash_node_s {
-    char *key;
-    int value;
-    struct hash_node_s *next;
+
+        char *key;
+        int value;
+        struct hash_node_s *next;
 } hash_node_t;
 
 typedef struct hash_table_s {
-    unsigned long int size;
-    hash_node_t **array;
+        unsigned long int size;
+        hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size) {
-    hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL) {
-        return NULL;
+        hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+        if (hash_table == NULL) {
+
+                return NULL;
     }
 
     hash_table->array = calloc(size, sizeof(hash_node_t *));
+
     if (hash_table->array == NULL) {
-        free(hash_table);
-        return NULL;
+
+            free(hash_table);
+	    
+            return NULL;
     }
 
     hash_table->size = size;
