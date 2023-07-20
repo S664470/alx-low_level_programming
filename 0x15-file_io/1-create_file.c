9613e569ea726c6c409fc2abc924f5acbbd8be9e
S664470 <shahialtayeb@gmail.com>
2023-07-20 00:25:29 -0700

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-/**
- * read_textfile - function that reads a text file and prints.
- * @filename: the name of the file
- * @letters: the number of letters it should read and print
- *
- * File: 0-read_textfile.c
- * Return: the actual number of letters it could read and print
- *	or 0 if fails
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	ssize_t o, r, w;
-	char *buf;
+int create_file(const char *filename, char *text_content) {
+    if (filename == NULL) {
+        return -1;
+    }
 
-	if (filename == NULL)
-		return (0);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        return -1;
+    }
 
-	buf = malloc(sizeof(char) * letters);
+    if (text_content != NULL) {
+        size_t len = strlen(text_content);
+        ssize_t bytes_written = write(fd, text_content, len);
+        if (bytes_written == -1 || (size_t) bytes_written != len) {
+            close(fd);
+            return -1;
+        }
+    }
 
-	if (buf == NULL)
-		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buf, letters);
-	w = write(STDOUT_FILENO, buf, r);
-
-	if (0 == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buf);
-		return (0);
-	}
-
-	close(o);
-
-	return (w);
+    close(fd);
+    return 1;
 }
