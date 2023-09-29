1c3c45d7faf2e4918206b4fc06fa182c6b0ee154
S664470 <shahialtayeb@gmail.com>
2023-09-29 07:52:12 -0700

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,42 +1,32 @@
-#include <stdlib.h>
-
-/**
- * File: hash_table_create
- * Auth: Shahinda Altayeb
- * Dias: Function to create hash table
- * @size: Is the size of the array
- * Return: Pointer to a new hash table or NULL if something wrong
+/*
+ * File: 0-hash_table_create.c
+ * Auth Michael Ndula
  */
 
-typedef struct hash_node_s {
+#include "hash_tables.h"
 
-        char *key;
-        int value;
-        struct hash_node_s *next;
-} hash_node_t;
+/**
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
 
-typedef struct hash_table_s {
-        unsigned long int size;
-        hash_node_t **array;
-} hash_table_t;
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-hash_table_t *hash_table_create(unsigned long int size) {
-        hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-        if (hash_table == NULL) {
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-                return NULL;
-    }
-
-    hash_table->array = calloc(size, sizeof(hash_node_t *));
-
-    if (hash_table->array == NULL) {
-
-            free(hash_table);
-	    
-            return NULL;
-    }
-
-    hash_table->size = size;
-
-    return hash_table;
+	return (ht);
 }
