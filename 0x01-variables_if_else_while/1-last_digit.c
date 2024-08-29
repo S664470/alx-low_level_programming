982068669db27753c2cb6a41230e67e74af23250
S664470 <shahialtayeb@gmail.com>
2024-08-29 01:11:33 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	else if (last_dig == 0)
 		printf("Last digit of %d is %d and is 0\n", n, last_dig);
 	else if (last_dig < 6 && last_dig != 0)
-		printf("Last digit of of %d is %d and is less than 6 and not 0\n"
+		printf("Last digit of %d is %d and is less than 6 and not 0\n"
 				, n, last_dig);
 
 	return (0);
