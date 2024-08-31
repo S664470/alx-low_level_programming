77100506bd6ffa8e4811be2ec97ef664fb7bfaed
S664470 <shahialtayeb@gmail.com>
2024-08-30 18:48:00 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isalpha - check for alphabet
+ * @c: the charecter to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (_isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
